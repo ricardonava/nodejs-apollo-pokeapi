16cb0e4fb0c6ae811c9332d74100179b489ea826
Ricardo <ricardo.a.nava@gmail.com>
2020-09-29 23:18:30 -0700

    Add type to Query args

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server'
 import PokeAPI from './dataSources'
+import typeDefs from './typeDefs'
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Pokemon" type defines the queryable fields for a pokemon in our data source.
-  type Pokemon {
-    name: String
-    id: Int
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "pokemon" query returns a Pokemon (defined above).
-  type Query {
-    pokemon(name: String!): Pokemon
-  }
-`
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves the pokemon from pokemon object above.
 const resolvers = {
   Query: {
-    pokemon: (root, { name }, { dataSources }) =>
-      dataSources.pokeAPI.getAPokemon(name),
+    // Destructure name from args
+    pokemon: (_root: any, { name }: { name: string }, { dataSources }: any) =>
+      dataSources.pokeAPI.getAPokemon({ name }),
   },
 }
 
